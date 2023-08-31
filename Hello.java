42cb78067868d85adef30b34a12adab6b2243f6c
JamesLyon <75685990+JamesWLyon@users.noreply.github.com>
2023-08-30 22:38:03 -0600

    Hello.java

---
+++ Hello.java
@@ -0,0 +1,20 @@
+/**
+ * Hello this is my Week 2 2B assignment
+ */
+
+public class Hello {
+    public static void main(String[] args) {
+
+        // These are the project varibles that store data {
+            
+            // This one holds the name that is printed in the console
+            var name = "James"; 
+            // This one contains the age that is as well printed in the console
+            var age = 16;
+
+        //}
+
+        // This is the println command, this prints a message in the console
+        System.out.println( "Hello world!\nMy name is " + name + " and I am " + age + " years old.");
+    }
+}
