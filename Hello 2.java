8bd09a689040749fad63413f85495837e088eb23
JamesLyon <75685990+JamesWLyon@users.noreply.github.com>
2023-08-30 22:53:05 -0600

    Hello 2.java

---
+++ Hello 2.java
@@ -0,0 +1,33 @@
+/**
+ * Hello this is my Week 2 2B assignment
+ */
+
+public class Hello {
+    public static void main(String[] args) {
+
+        // These are the project varibles that store data {
+            
+            // This one holds the name that is printed in the console
+            var name = "James"; 
+            // This one contains the age that is as well printed in the console
+            var age = 16;
+            // This is the names of my parents (the top two), as well as my siblings
+                // Father
+                var father = "Kurt";
+                // Mother
+                var mother = "Kristin";
+                // Oldest sister
+                var sister1 = "Madison";
+                // Middle sister
+                var sister2 = "Chloe";
+                // Brother
+                var brother = "Grant";
+                // Youngest sister
+                var sister3 = "Evelyn";
+
+        //}
+
+        // This is the println command, this prints a message in the console
+        System.out.println( "\nHello world!\nMy name is " + name + " and I am " + age + " years old.\n\nI also have my two parents...\n" + "My father " + father + ",\nand my mother " + mother + ".\n\nI also have four siblings...\nMy sister " + sister1 + ",\nmy sister " + sister2 + ",\nmy brother " + brother + ",\nand my sister " + sister3);
+    }
+}
