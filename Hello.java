dbddd07395a281b229c7371a05bf86b69734cdbe
JamesLyon <75685990+JamesWLyon@users.noreply.github.com>
2023-09-01 09:18:03 -0600

    Hello.java

--- Hello.java
+++ Hello.java
@@ -11,10 +11,23 @@ public class Hello {
             var name = "James"; 
             // This one contains the age that is as well printed in the console
             var age = 16;
+            // This is the names of my parents (the top two), as well as my siblings
+                // Father
+                var father = "Kurt";
+                // Mother
+                var mother = "Kristin";
+                // Oldest sister
+                var sister1 = "Madison";
+                // Middle sister
+                var sister2 = "Chloe";
+                // Brother
+                var brother = "Grant";
+                // Youngest sister
+                var sister3 = "Evelyn";
 
         //}
 
         // This is the println command, this prints a message in the console
-        System.out.println( "Hello world!\nMy name is " + name + " and I am " + age + " years old.");
+        System.out.println( "\nHello world!\nMy name is " + name + " and I am " + age + " years old.\n\nI also have my two parents...\n" + "My father " + father + ",\nand my mother " + mother + ".\n\nI also have four siblings...\nMy sister " + sister1 + ",\nmy sister " + sister2 + ",\nmy brother " + brother + ",\nand my sister " + sister3);
     }
 }
