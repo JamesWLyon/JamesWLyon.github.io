56bd8c3dce106327eebe2fd164798b562507fb1b
JamesLyon <75685990+JamesWLyon@users.noreply.github.com>
2023-09-14 14:54:49 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // Varibles {
     
-var navBtn = 0, dropArrow = 0, navDropNum = 0, deviceCheck;
+var navBtn = 0, dropArrow = 0, navDropNum = 0, mobileCheck;
 var navDrop = document.getElementsByClassName("drop-menu-img");
 var dropMenu = document.getElementsByClassName("drop-menu");
 var grade10 = document.getElementsByClassName("grade-10");
@@ -10,32 +10,32 @@ var grade10 = document.getElementsByClassName("grade-10");
 // Runs when website is loaded and resized {
 
 function WebsiteLoad (w) {
-w = window.innerHeight - ((window.innerWidth / 100) * 18);
-document.getElementsByClassName("nav-wrap")[0].style.height = w + "px";
+    w = window.innerHeight - ((window.innerWidth / 100) * 18);
+    document.getElementsByClassName("nav-wrap")[0].style.height = w + "px";
 
-// Sets the background for all of my 10th grade pages
-for (var i = 0; i < grade10.length; i++) {
-    grade10[i].style.backgroundImage = "url(https://i.imgur.com/WWQUhKl.png)";
-    grade10[i].style.backgroundSize = "120vw";
-    grade10[i].innerHTML = "<span class='wrapper'>Week " + (i + 1) + "</span>";
-}
+    // Sets the background for all of my 10th grade pages
+    for (var i = 0; i < grade10.length; i++) {
+        grade10[i].style.backgroundImage = "url(https://i.imgur.com/WWQUhKl.png)";
+        grade10[i].style.backgroundSize = "120vw";
+        grade10[i].innerHTML = "<span class='wrapper'>Week " + (i + 1) + "</span>";
+    }
 }
 
 //}
 
-// Checks if device is a mobile or not {
-    
+// Checks if user is on a mobile device {
+
 function isMobile() {
 
-    deviceCheck = false;
+    mobileCheck = false;
 
     (function(a){
       if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) 
-        deviceCheck = true;
+        mobileCheck = true;
     })(navigator.userAgent||navigator.vendor||window.opera);
-
-    if (deviceCheck == true) {
-        document.getElementsByName("head").innerHTML += '<link rel="stylesheet" type="text/css" href="mobile.css">';
+    
+    if (mobileCheck == true) {
+        document.getElementsByName("head").innerHTML += "<style>#nav-mobil-btn{z-index:100000;display:block;position:fixed;width:15vw;min-width:170px;max-width:200px;height:15vw;min-height:170px;max-height:200px;margin:2.5vw;border-radius:100%;background-color:rgb(0,0,0,0.5);transition:.3s;}/*Changesyourpointertocursorwhenhoveringoverit*/#nav-mobil-btn:hover{cursor:pointer;}#nav-mobil-btn.wrapper{display:flex;flex-direction:column;flex-wrap:wrap;width:100%;height:100%;border-radius:100%;justify-content:center;align-items:center;}/*Stylesthelinesinthebutton*/.line{display:block;position:relative;width:50%;height:5%;margin:6%;background-color:#EEE;transition:.3s;}.toggle.one{transform:translateY(2.9vw)rotate(45deg);}.toggle.two{width:0;opacity:0;}.toggle.three{transform:translateY(-2.9vw)rotate(-45deg);}/*Stylesthenavmenuformobile*/#nav-menu{z-index:90000;position:fixed;display:flex;flex-direction:row;flx-wrap:wrap;justify-content:center;alight-items:top;text-align:center;font:4.5vw'Arial';}.resources.item{width:65%;height:auto;padding:8%;margin-bottom:8vw;border-radius:3vw;color:#EEE;background-color:rgb(27,27,27);}.resourcesa{display:block;color:#EEE;font:5vw'Comfortaa';margin-bottom:4vw;}/*Typingeffect*/.container{display:flex;justify-content:center;align-items:center;}.page-title.containerp{font-size:3rem;padding:0.5rem;font-weight:bold;letter-spacing:0.1rem;text-align:center;overflow:hidden;}.page-title.containerspan.cursor{position:relative;display:inline-block;background-color:white;margin-left:0.09rem;width:3px;height:10vw;top:2.5vw;animation:blink1sinfinite;}.page-title.containerspan.cursor.typing{animation:none;}@keyframesblink{0%{background-color:white;}49%{background-color:white;}50%{background-color:transparent;}99%{background-color:transparent;}100%{background-color:white;}}</style>";
     }
 };
 
@@ -46,31 +46,31 @@ function isMobile() {
 
 function NavBtnMobil (w, i , item) {
 
-// This gets the nav menu button from the html file
-item = document.getElementById("nav-mobil-btn");
-// This gets the width of the nav menu button
-i = item.getBoundingClientRect();
-// With a little math this calculates where to perfectly position the nav menu button when you click it no matter the size of your phone
-w = (window.innerWidth - i.width) - ((window.innerWidth/100) * 2.5);
+    // This gets the nav menu button from the html file
+    item = document.getElementById("nav-mobil-btn");
+    // This gets the width of the nav menu button
+    i = item.getBoundingClientRect();
+    // With a little math this calculates where to perfectly position the nav menu button when you click it no matter the size of your phone
+    w = (window.innerWidth - i.width) - ((window.innerWidth/100) * 2.5);
 
-// This is an if/else statement that changes the style of the nav menu button, as well as show and hide the actual menu 
-if (navBtn == 0) {
-    navBtn = 1;
-    document.getElementById("nav-mobil-btn").style.marginLeft = w + "px";
-    document.getElementById("nav-mobil-btn").style.backgroundColor = "rgb(0, 0, 0, 0)";
-    document.getElementById("nav-menu").style.left = "0";
-    document.getElementById("nav-menu-icons").style.left = "0";
-} else {
-    navBtn = 0;
-    document.getElementById("nav-mobil-btn").style.marginLeft = "2.5vw";
-    document.getElementById("nav-mobil-btn").style.backgroundColor = "rgb(0, 0, 0, 0.5)";
-    document.getElementById("nav-menu").style.left = "-100vw";
-    document.getElementById("nav-menu-icons").style.left = "-100vw";
-}
+    // This is an if/else statement that changes the style of the nav menu button, as well as show and hide the actual menu 
+    if (navBtn == 0) {
+        navBtn = 1;
+        document.getElementById("nav-mobil-btn").style.marginLeft = w + "px";
+        document.getElementById("nav-mobil-btn").style.backgroundColor = "rgb(0, 0, 0, 0)";
+        document.getElementById("nav-menu").style.left = "0";
+        document.getElementById("nav-menu-icons").style.left = "0";
+    } else {
+        navBtn = 0;
+        document.getElementById("nav-mobil-btn").style.marginLeft = "2.5vw";
+        document.getElementById("nav-mobil-btn").style.backgroundColor = "rgb(0, 0, 0, 0.5)";
+        document.getElementById("nav-menu").style.left = "-100vw";
+        document.getElementById("nav-menu-icons").style.left = "-100vw";
+    }
 
-// This also adds a class to the wrapper div in the nav menu button
-document.getElementsByClassName("wrapper")[0].classList.toggle("toggle");
-}
+    // This also adds a class to the wrapper div in the nav menu button
+    document.getElementsByClassName("wrapper")[0].classList.toggle("toggle");
+    }
 
 //}
 
@@ -78,22 +78,22 @@ document.getElementsByClassName("wrapper")[0].classList.toggle("toggle");
 
 function NavDrop (i, itm) {
 
-// Sets the navDropNum variable to whatever the correct arrow number is
-navDropNum = i;
-// This sets the varible item to the class of that specific arrow
-let item = navDrop[i];
-// This gets the actual drop menu part
-itm = dropMenu[i];
+    // Sets the navDropNum variable to whatever the correct arrow number is
+    navDropNum = i;
+    // This sets the varible item to the class of that specific arrow
+    let item = navDrop[i];
+    // This gets the actual drop menu part
+    itm = dropMenu[i];
 
-if (dropArrow == 0) {
-    dropArrow = 1;
-    item.style.transform = "rotate(90deg)";
-    itm.style.height = "100%";
-} else {
-    dropArrow = 0;
-    item.style.transform = "rotate(-0deg)";
-    itm.style.height = "0";
-}
+    if (dropArrow == 0) {
+        dropArrow = 1;
+        item.style.transform = "rotate(90deg)";
+        itm.style.height = "100%";
+    } else {
+        dropArrow = 0;
+        item.style.transform = "rotate(-0deg)";
+        itm.style.height = "0";
+    }
 }
 
 //}
@@ -102,17 +102,17 @@ if (dropArrow == 0) {
 
 function ChangePage (index) {
 
-// Resets any drop menus
-dropArrow = 1;
-NavDrop(navDropNum);
+    // Resets any drop menus
+    dropArrow = 1;
+    NavDrop(navDropNum);
 
-// Changes the website page
-var page = document.getElementsByClassName('page');
-for (var i = 0; i < page.length; i++) {
-    page[i].style.display = "none";
-}
-page[index].style.display = "block";
-window.scroll(0, 0);   
+    // Changes the website page
+    var page = document.getElementsByClassName('page');
+    for (var i = 0; i < page.length; i++) {
+        page[i].style.display = "none";
+    }
+    page[index].style.display = "block";
+    window.scroll(0, 0);   
 }
 ChangePage(0);
 
@@ -122,15 +122,15 @@ ChangePage(0);
 
 function GetProYears (y, item) {
 
-// Gets the current year and finds out how many years it has been since 2018
-y = (new Date().getFullYear()) - 2018;
-// Gets the items we add this information to
-item = document.getElementsByClassName("programming-years");
+    // Gets the current year and finds out how many years it has been since 2018
+    y = (new Date().getFullYear()) - 2018;
+    // Gets the items we add this information to
+    item = document.getElementsByClassName("programming-years");
 
-// Adds y to the items
-for (var i = 0; i < item.length; i++) {
-    item[i].textContent = y;
-}
+    // Adds y to the items
+    for (var i = 0; i < item.length; i++) {
+        item[i].textContent = y;
+    }
 }
 
 //}
