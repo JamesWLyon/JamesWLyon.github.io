6f6a4ac40601652047c31dd39ab8dc3593ee64e1
JamesLyon <75685990+JamesWLyon@users.noreply.github.com>
2023-10-19 20:37:40 -0600

    Create Cars.java

---
+++ Cars.java
@@ -0,0 +1,8 @@
+// The cars class
+public class Cars {
+    String brand; // Car's brand
+    int year = 2000; // Car's year it was made
+    int horsePower = 1000; // Car's horse power
+    String owner; // Car's owner
+    int cost; // Car's cost to buy
+}
