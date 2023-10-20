e6ec4b345b666621866cad8e397723edef79d5b4
JamesLyon <75685990+JamesWLyon@users.noreply.github.com>
2023-10-19 20:37:19 -0600

    Create People.java

---
+++ People.java
@@ -0,0 +1,7 @@
+// The people class
+public class People {
+    String name = "blank"; // Person's name
+    int age = 50; // Person's age
+    String height; // Person's height
+    int weight = 100; // Person's weight
+}
